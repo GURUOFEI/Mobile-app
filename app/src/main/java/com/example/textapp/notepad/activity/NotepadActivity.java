4705fef67dd6358d4daff71b872a91640847d5fe
GURUOFEI <guruofei20001219@163.com>
2023-04-23 16:08:14 +0800

    1. Notebook List add classify filter;

--- app/src/main/java/com/example/textapp/notepad/activity/NotepadActivity.java
+++ app/src/main/java/com/example/textapp/notepad/activity/NotepadActivity.java
@@ -1,12 +1,18 @@
 package com.example.textapp.notepad.activity;
 
+import android.animation.ValueAnimator;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
@@ -27,16 +33,34 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NotepadActivity extends AppCompatActivity {
+public class NotepadActivity extends BaseActivity {
     private ListView listView;
-    private SQLiteHelper mSQLiteHelper;
+    //    private SQLiteHelper mSQLiteHelper;
     private List<NotepadBean> list;
     NotepadAdapter adapter;
+
+    private TextView note_name;
 
     /**
      * 当前用户的uuid
      */
     private String uuid;
+
+    /**
+     * 当前列表分类
+     * -1. all
+     * 0. person
+     * 1. life
+     * 2. work
+     * 3. finance
+     * 4. other
+     */
+    private int type = -1;
+
+    private PopupWindow menuPopupWindow;
+    private RelativeLayout rl_title;
+
+    private String[] typeArray = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,11 +90,69 @@ public class NotepadActivity extends AppCompatActivity {
                 startActivity(new Intent(NotepadActivity.this, EditMineActivity.class));
             }
         });
+
+        //标题相关
+        note_name = findViewById(R.id.note_name);
+        rl_title = findViewById(R.id.rl_title);
+        note_name.setOnClickListener(v -> showType());
+        chooseType(-1);
+    }
+
+    /**
+     * 显示分类选择菜单
+     */
+    private void showType() {
+        if (menuPopupWindow == null) {
+            View view = getLayoutInflater().inflate(R.layout.type_menu, null);
+            TextView tv_all = (TextView) view.findViewById(R.id.tv_all);
+            tv_all.setOnClickListener(v -> chooseType(-1));
+            TextView tv_person = (TextView) view.findViewById(R.id.tv_person);
+            tv_person.setOnClickListener(v -> chooseType(0));
+            TextView tv_life = (TextView) view.findViewById(R.id.tv_life);
+            tv_life.setOnClickListener(v -> chooseType(1));
+            TextView tv_work = (TextView) view.findViewById(R.id.tv_work);
+            tv_work.setOnClickListener(v -> chooseType(2));
+            TextView tv_finance = (TextView) view.findViewById(R.id.tv_finance);
+            tv_finance.setOnClickListener(v -> chooseType(3));
+            TextView tv_other = (TextView) view.findViewById(R.id.tv_other);
+            tv_other.setOnClickListener(v -> chooseType(4));
+
+            menuPopupWindow = new PopupWindow(view, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+            menuPopupWindow.setOutsideTouchable(true);
+            menuPopupWindow.setAnimationStyle(R.style.popmenu_animation);
+        }
+        if (menuPopupWindow.isShowing()) {
+            menuPopupWindow.dismiss();
+        } else {
+            menuPopupWindow.showAsDropDown(note_name);
+        }
+    }
+
+    /**
+     * 选择分类
+     */
+    private void chooseType(int chooseType) {
+        //根据传入的分类显示标题
+        String typeStr = getString(R.string.type_all);
+        this.type = chooseType;
+        if (chooseType != -1) {
+            typeStr = typeArray[type];
+        }
+        //设置标题
+        note_name.setText(String.format(getString(R.string.title), typeStr));
+        //隐藏菜单
+        if (menuPopupWindow != null) {
+            menuPopupWindow.dismiss();
+        }
+        getData();
     }
 
     public void initData() {
         uuid = (String) SharedPreUtil.getParam(NotepadActivity.this, SharedPreUtil.LOGIN_UUID, "");
-        mSQLiteHelper = new SQLiteHelper(this);
+        //读取分类数据
+        typeArray = getResources().getStringArray(R.array.noteboot_type);
+
+//        mSQLiteHelper = new SQLiteHelper(this);
         showQueryData();
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -126,42 +208,35 @@ public class NotepadActivity extends AppCompatActivity {
 
         //这里改为查询Firestore Database
 //        list=mSQLiteHelper.query((String) SharedPreUtil.getParam(NotepadActivity.this, SharedPreUtil.LOGIN_DATA,""));
-//        FirestoreDatabaseUtil.getInstance()
-//                .getUserNotebook(uuid)
-//                .get()
-//                .addOnCompleteListener(task -> {
-//                    if (task.isSuccessful()) {//请求成功
-//                        //把数据拿转换成NotepadBean并放到list
-//                        for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
-//                            NotepadBean notepadBean = new NotepadBean(queryDocumentSnapshot.getId(), queryDocumentSnapshot.getData());
-//                            list.add(notepadBean);
-//                        }
-//                        //刷新列表
-//                        adapter.notifyDataSetChanged();
-//                    }
-//                });
-
         //添加一个监听
+        getData();
+    }
+
+    private void getData() {
         FirestoreDatabaseUtil.getInstance()
                 .getUserNotebook(uuid)
-                .addSnapshotListener(new EventListener<QuerySnapshot>() {
-                    @Override
-                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                        if (e != null) {
-                            LogUtil.d("Listen failed." + e.getMessage());
-                            return;
-                        }
+                .addSnapshotListener((queryDocumentSnapshots, e) -> {
+                    if (e != null) {
+                        return;
+                    }
 
-                        list.clear();
-                        for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
-                            //重新添加数据
-                            NotepadBean notepadBean = new NotepadBean(doc.getId(), doc.getData());
-                            LogUtil.d(doc.get("notepadContent").toString());
+                    list.clear();
+                    for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+                        //重新添加数据
+                        NotepadBean notepadBean = new NotepadBean(doc.getId(), doc.getData());
+                        //判断是否选择了分类,-1表示显示全部
+                        if (type != -1) {
+                            //只显示该分类
+                            if (notepadBean.getType() == type) {
+                                list.add(notepadBean);
+                            }
+                        } else {
+                            //显示全部
                             list.add(notepadBean);
                         }
-                        //刷新列表
-                        adapter.notifyDataSetChanged();
                     }
+                    //刷新列表
+                    adapter.notifyDataSetChanged();
                 });
     }
 
